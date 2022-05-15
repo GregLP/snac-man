48ab56f8e3de02d7903da1b325d42aa6c7bfb443
greg <greglp@users.noreply.github.com>
2022-05-15 16:02:39 -0700

    Added svg
    Added  new svgs for button icons. Added some comments to the js file draw.js.

--- assets/js/draw.js
+++ assets/js/draw.js
@@ -1,3 +1,4 @@
+//create the url for the puzzle ?name=puzzleName
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -5,37 +6,40 @@ function getParameterByName(name) {
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-var puzzle = getParameterByName("name");
-var gameSongs = games[puzzle]["puzzle"];
-$("#munchers").text(games[puzzle]["title"]);
+var puzzle = getParameterByName("name"); // Declare a new variable puzzle.  Assign it the value returned by the getParameterByName function with "name" passed as argument
+var gameSongs = games[puzzle]["puzzle"]; // Delcare new variable gameSongs. Assign it the value of the property puzzle from the object games
+$("#munchers").text(games[puzzle]["title"]); // jQuery Target the html element with the id #munchers and replace the content with the value of the property title from the object games
 
-var songs = Object.keys(gameSongs);
+var songs = Object.keys(gameSongs); // Declare a new variable songs. Assign it the value of the Object.keys() method with gameSongs passed as an argument
 
-rows = 4;
-cols = 5;
-var progressTotalHeight = window.innerHeight/1.8;
-var progress_left_offset = window.innerWidth/16;
-var progress_top_offset = window.innerHeight/1.26;
-var left_offset = window.innerWidth/4;
-var top_offset = window.innerHeight/9.5;
-var dim = window.innerHeight/cols;
-spacePos = "00";
-badGuyPos1 = "33";
-//badGuyPos2 = "33";
-var lives = 3;
-var numCorrect = 0;
+rows = 4; //Assign the variable rows the value of 4
+cols = 5; //Assign the variable cols the value of 5
+var progressTotalHeight = window.innerHeight/1.8; // Declare a new variable progressTotalHeight and assign it the value of the window height divided by 1.8
+var progress_left_offset = window.innerWidth/16; // Declare a new variable progress_left_offset and assign it the value of the window width divided by 16
+var progress_top_offset = window.innerHeight/1.26; // Declare a new variable progress_top_offset and assign it the value of the window height divided by 1.26
+var left_offset = window.innerWidth/4; // Declare a new variable left_offset and assign it the value of the window width divided by 1.8
+var top_offset = window.innerHeight/9.5; // Declare a new variable top_offset and assign it the value of the window height divided by 9.5
+var dim = window.innerHeight/cols; // Declare a new variable dim and assign it the value of the window height divided by the variable cols
+spacePos = "00"; //Assign a value of 00 to the variable spacePos
+badGuyPos1 = "33"; //Assign a value of 33 to the variable badGuyPos1
+//badGuyPos2 = "33"; Commented out position for badGuy2
+var lives = 3; //Declare a new variable lives and assign it the value of 3
+var numCorrect = 0; //Declare a new variable of numCorrect and assign it the value of 0
+
+//Create a new function findNumToWin(). Checks reamining correct answers in puzzle. Looks at puzzle object and counts the number of 1's remaining. Adds it to a new local variable numToWin
 var findNumToWin = function(obj){
 	var numToWin = 0;
 	for (var key in obj){
-		numToWin += (obj.hasOwnProperty(key) && obj[key]) ? 1 : 0; 
+		numToWin += (obj.hasOwnProperty(key) && obj[key]) ? 1 : 0;
 	}
-	return numToWin; 
-}
+	return numToWin;
+};
 
-numToWin = findNumToWin(gameSongs);
+numToWin = findNumToWin(gameSongs); //Assign a variable numToWin with the return value of the function findNumToWin()
 
-document.write("<div id=\"celebrate\"><h2>You Win!</h2></div>");
+document.write("<div id=\"celebrate\"><h2>You Win!</h2></div>"); //Writes a celebration message on the page in the celebrate div
 
+//Create new function drawLives() that draws snacman on the right of the board. Calculates lives remaining.
 var drawLives = function(lives){
 	var lives_offset_left = left_offset + cols*dim + dim/2;
 	//var lives_offset_top = top_offset + rows*dim/2;
@@ -45,6 +49,7 @@ var drawLives = function(lives){
 	}
 }
 
+//Create new function drawPrizeClicks() that draws circles on the top right of the board.
 var drawPrizeClicks = function(prizes){
 	var prizes_offset_left = left_offset + cols*dim + dim/2;
 	//var prizeArray = ["snacBadGuy.png", "snacRemove.png", "snacTime.png"];
@@ -54,25 +59,30 @@ var drawPrizeClicks = function(prizes){
 	}
 }(3);
 
-document.write("<div id=\"progress_label\" style=\"top:" + progress_top_offset/0.98 + "px; left:" + progress_left_offset + "px; height:" + dim/7.7 + 
+
+// Writes the numbers to win below the progress bar
+document.write("<div id=\"progress_label\" style=\"top:" + progress_top_offset/0.98 + "px; left:" + progress_left_offset + "px; height:" + dim/7.7 +
 		"px; width:" + dim/1.9 + "px;\"><em>" + numToWin + " To Win</em></div>");
 
-document.write("<div id=\"progress\" style=\"top:" + progress_top_offset + "px; left:" + progress_left_offset + "px; height:" + 0 + 
+//writes the green progress bar that overwrites the empty progress bar
+document.write("<div id=\"progress\" style=\"top:" + progress_top_offset + "px; left:" + progress_left_offset + "px; height:" + 0 +
 		"px; width:" + dim/2 + "px;\"></div>");
 
-document.write("<div id=\"empty_progress\" style=\"top:" + (progress_top_offset - progressTotalHeight) + "px; left:" + progress_left_offset + "px; height:" + progressTotalHeight + 
+//writes the full progress bar with red bg
+document.write("<div id=\"empty_progress\" style=\"top:" + (progress_top_offset - progressTotalHeight) + "px; left:" + progress_left_offset + "px; height:" + progressTotalHeight +
 		"px; width:" + dim/2 + "px;\"></div>");
 
+//Draws the actual gameboard
 var drawBoard = function(){
 	var left_offset = window.innerWidth/4;
 	var top_offset = window.innerHeight/8.5;
-	document.write("<div id=\"board\" style=\" top:" + top_offset + "px; left:" + left_offset + "px; height:" + window.innerHeight/1.5 + 
+	document.write("<div id=\"board\" style=\" top:" + top_offset + "px; left:" + left_offset + "px; height:" + window.innerHeight/1.5 +
 		"px; width:" + window.innerWidth/1.25 + "px;\">");
 	for (var i = 0; i < rows; i++){
 		left_offset = window.innerWidth/4;
 		for (var j = 0; j < cols; j++){
-			document.write("<div id=\"" + i.toString() + "r" + j.toString() + "\" class=\"cell\" style=\"top:" + 
-				top_offset +"px; left: " + left_offset + "px; height:" + dim +"px; width:" + dim + "px;\">" + "<p>" + songs.pop() + 
+			document.write("<div id=\"" + i.toString() + "r" + j.toString() + "\" class=\"cell\" style=\"top:" +
+				top_offset +"px; left: " + left_offset + "px; height:" + dim +"px; width:" + dim + "px;\">" + "<p>" + songs.pop() +
 				"</p>" + "</div>");
 			left_offset += dim;
 		}
@@ -83,6 +93,8 @@ var drawBoard = function(){
 	document.write("<img id=\"badGuy1\" src=\"assets/img/tvbadguy.png\" />");
 };
 
+
+//Creates new function placeSpaceship() that adds the ufo to the game
 var placeSpaceship = function(ufo, id){
 	if (id == "test")
 		spacePos = ufo;
@@ -93,6 +105,7 @@ var placeSpaceship = function(ufo, id){
 	document.getElementById(id).style.top = new_top_offset + "px";
 };
 
+//Creates new function placeBadGuy() that adds badguy to game
 var placeBadGuy = function(ufo, id){
 	if (id == "badGuy1")
 		badGuyPos1 = ufo;
@@ -105,6 +118,7 @@ var placeBadGuy = function(ufo, id){
 	document.getElementById(id).style.top = new_top_offset + "px";
 };
 
+//Creates a new function placePrizes() that adds the prizes to the game 
 var placePrizes = function(prizes){
 	var pos = "";
 	var id = "";
