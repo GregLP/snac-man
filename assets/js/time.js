b630cb476551369133cdff0d22e7696c9c374ac5
greg <greglp@users.noreply.github.com>
2022-05-20 19:36:23 -0700

    Hoisted JS variables in timer. Added some styles to the gameboard

--- assets/js/time.js
+++ assets/js/time.js
@@ -4,15 +4,13 @@ var ten_seconds = 0;
 var seconds = 0;
 var blinking = false;
 var blink;
+var clock = setInterval(function(){setTime(ten_minutes, minutes, ten_seconds, --seconds)}, 1000);
 
 var setTime = function(new_ten_minutes, new_minutes, new_ten_seconds, new_seconds){
 	ten_minutes = new_ten_minutes;
 	minutes = new_minutes;
 	ten_seconds = new_ten_seconds;
 	seconds = new_seconds;
-
-	//if (ten_minutes == 0 && minutes == 0 && ten_seconds == 0)
-		//$('#timer').css("color", "red");
 
 	if ((!blinking && ten_minutes == 0 && minutes == 0 && ten_seconds == 0 ) || (!blinking && minutes == 0 && ten_seconds <= 1 && seconds == 0)){
 		blinking = true;
@@ -36,13 +34,14 @@ var setTime = function(new_ten_minutes, new_minutes, new_ten_seconds, new_second
 			minutes--;
 		}
 		else {
+			seconds = 0;
+			$('#timer').css("visibility", "visible");
 			alert("Game Over!");
 			clearInterval(clock);
 			clearInterval(blink);
-			seconds = 0;
+
 		}
 	}
-	$("#timer").text(ten_minutes.toString() + minutes.toString() + ":" + ten_seconds.toString() + seconds.toString());
-}
 
-var clock = setInterval(function(){setTime(ten_minutes, minutes, ten_seconds, --seconds)}, 1000);
+	$("#timer").text(ten_minutes.toString() + minutes.toString() + ":" + ten_seconds.toString() + seconds.toString());
+};
