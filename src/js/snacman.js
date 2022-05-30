0c06dc5c97d5c15aa421c86bd7bf858dc0dbf1b6
greg <greglp@users.noreply.github.com>
2022-05-29 23:52:47 -0700

    updated svg classes to be consistent

--- src/js/snacman.js
+++ src/js/snacman.js
@@ -62,7 +62,7 @@ const drawBoard = function(){
 
 function placeSnacman() {
     const snacmanStartingCell = document.getElementById("r0c0");
-    const drawSnacman = `<svg id="snacman" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"  viewBox="0 0 30.3 37.1" style="enable-background:new 0 0 30.3 37.1;" xml:space="preserve"><line class="line line-1 leg left-leg" x1="13.7" y1="31.3" x2="13.7" y2="37.1"/><line class="line line-2 foot left-foot" x1="8.6" y1="36.9" x2="13.9" y2="36.9"/><line class="line line-3 leg right-leg" x1="18" y1="31.4" x2="18" y2="37.1"/><line class="line line-4 foot right-foot" x1="17.8" y1="36.9" x2="23.1" y2="36.9"/><path class="st1 path path-1 hamburger hamburger-top" d="M29.7,11.3C28.6,5.2,22.5,0.5,15.2,0.5S1.8,5.2,0.6,11.3H29.7z"/><path class="st1 path path-2 hamburger hamburger-bottom" d="M1.5,22.5c2.3,5.2,7.6,8.9,13.7,8.9s11.4-3.7,13.7-8.9H1.5z"/><path class="st2 path path-3 eye eye-left eye-whites" d="M14.2,3.8c0,1.1-0.9,2-1.9,2s-1.9-0.9-1.9-2s0.9-2,1.9-2S14.2,2.6,14.2,3.8z"/><path class="st2 path path-4 eye eye-right eye-whites" d="M20.7,3.6c0,1.1-0.9,2-1.9,2s-1.9-0.9-1.9-2s0.9-2,1.9-2S20.7,2.5,20.7,3.6z"/><path class="st3 path path-5 eye eye-right eye-pupil" d="M20.3,4.4c0,0.6-0.5,1.2-1.1,1.2s-1.1-0.5-1.1-1.2s0.5-1.2,1.1-1.2S20.3,3.8,20.3,4.4z"/><path class="st3 path path-6 eye eye-left eye-pupil" d="M12.9,3.1c0,0.6-0.5,1.1-1.1,1.1s-1.1-0.5-1.1-1.2S11.2,2,11.8,2S12.9,2.5,12.9,3.1z"/></svg>`;
+    const drawSnacman = `<svg id="snacman" x="0px" y="0px"  viewBox="0 0 30.3 37.1" style="enable-background:new 0 0 30.3 37.1;" xml:space="preserve"><g class="limb limb-left"><line class="line-1 leg" x1="13.7" y1="31.3" x2="13.7" y2="37.1"/><line class="line-2 foot" x1="8.6" y1="36.9" x2="13.9" y2="36.9"/></g><g class="limb limb-right"><line class="line-3 leg" x1="18" y1="31.4" x2="18" y2="37.1"/><line class="line-4 foot" x1="17.8" y1="36.9" x2="23.1" y2="36.9"/></g><g class="mouth"><path class="path-1 mouth-top" d="M29.7,11.3C28.6,5.2,22.5,0.5,15.2,0.5S1.8,5.2,0.6,11.3H29.7z"/><path class="path-2 mouth-bottom" d="M1.5,22.5c2.3,5.2,7.6,8.9,13.7,8.9s11.4-3.7,13.7-8.9H1.5z"/></g><g class="eye eye-left"><path class="path-3 eye-white" d="M14.2,3.8c0,1.1-0.9,2-1.9,2s-1.9-0.9-1.9-2s0.9-2,1.9-2S14.2,2.6,14.2,3.8z"/><path class="path-4 eye-pupil" d="M12.9,3.1c0,0.6-0.5,1.1-1.1,1.1s-1.1-0.5-1.1-1.2S11.2,2,11.8,2S12.9,2.5,12.9,3.1z"/></g><g class="eye eye-right"><path class="path-5 eye-white" d="M20.7,3.6c0,1.1-0.9,2-1.9,2s-1.9-0.9-1.9-2s0.9-2,1.9-2S20.7,2.5,20.7,3.6z"/><path class="path-6 eye eye-pupil" d="M20.3,4.4c0,0.6-0.5,1.2-1.1,1.2s-1.1-0.5-1.1-1.2s0.5-1.2,1.1-1.2S20.3,3.8,20.3,4.4z"/></g></svg>`;
     snacmanStartingCell.insertAdjacentHTML("beforeend", drawSnacman);
 }
 
@@ -110,6 +110,7 @@ drawLives(2);
 drawBoard();
 placeSnacman();
 addPuzzleTitle()
+
 
 let currentRow = 0;
 let currentColumn = 0;
@@ -179,16 +180,16 @@ window.addEventListener("keydown", function (event) {
 
 
 const eat = function() {
-    const leftLeg = document.querySelector('.left-leg');
-    const rightLeg = document.querySelector('.right-leg');
-    const hamburgerBottom = document.querySelector('.hamburger-bottom');
+    const leftLeg = document.querySelector('.limb-left .leg');
+    const rightLeg = document.querySelector('.limb-right .leg');
+    const mouthBottom = document.querySelector('.mouth-bottom');
     leftLeg.setAttribute('y1', '21.5');
     rightLeg.setAttribute('y1', '21.5');
-    hamburgerBottom.setAttribute('d', 'M1.5,12.5c2.3,5.2,7.6,8.9,13.7,8.9s11.4-3.7,13.7-8.9H1.5z');
+    mouthBottom.setAttribute('d', 'M1.5,12.5c2.3,5.2,7.6,8.9,13.7,8.9s11.4-3.7,13.7-8.9H1.5z');
     setTimeout(function(){
         leftLeg.setAttribute('y1', '31.3');
         rightLeg.setAttribute('y1', '31.3');
-        hamburgerBottom.setAttribute('d', 'M1.5,22.5c2.3,5.2,7.6,8.9,13.7,8.9s11.4-3.7,13.7-8.9H1.5z');
+        mouthBottom.setAttribute('d', 'M1.5,22.5c2.3,5.2,7.6,8.9,13.7,8.9s11.4-3.7,13.7-8.9H1.5z');
     }, 100)
 }
 
