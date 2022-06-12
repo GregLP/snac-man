0c4db185f9357f2bf86edf98c7af5f61ded8139f
greg <greglp@users.noreply.github.com>
2022-06-12 16:21:10 -0700

    Touched up more styles for the games played table

--- src/js/modules/player.js
+++ src/js/modules/player.js
@@ -1,10 +1,11 @@
 let allGames = Object.values(JSON.parse(localStorage.getItem('playerGames')));
+document.getElementById('totalScore').textContent = localStorage.getItem('totalUserScore');
 
 for (let i = 0; i < allGames.length; i++) {
     let gameValue = allGames[i];
     if (gameValue.hasOwnProperty('played')) {
         let tableRow = document.createElement('tr');
-        tableRow.setAttribute('id', `results${i}`);
+        tableRow.setAttribute('class', `game-result`);
         tableRow.appendChild(createHeader(allGames[i]['title']));
         tableRow.appendChild(createWinLossPara(allGames[i]['result']));
         tableRow.appendChild(createScorePara(allGames[i]['score']));
@@ -13,13 +14,15 @@ for (let i = 0; i < allGames.length; i++) {
 }
 
 function createHeader(title) {
-    let heading = document.createElement('th');
+    let heading = document.createElement('td');
+    heading.setAttribute('scope', 'col');
     heading.innerHTML = title;
     return heading;
 }
 
 function createWinLossPara(result) {
-    let resultPara = document.createElement('th');
+    let resultPara = document.createElement('td');
+    resultPara.setAttribute('scope', 'col');
     if (result === 'win') {
         resultPara.innerHTML = `<svg class="win-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M9.55 19 2.825 12.275 5.25 9.85 9.55 14.175 18.775 4.95 21.2 7.35Z"/></svg>`;
     } else {
@@ -29,14 +32,32 @@ function createWinLossPara(result) {
 }
 
 function createScorePara(score) {
-    let scorePara = document.createElement('th');
+    let scorePara = document.createElement('td');
+    scorePara.setAttribute('scope', 'col');
     scorePara.innerHTML = score;
     return scorePara;
 }
 
-
-function setUserScore() {
-    document.getElementById('totalScore').textContent = localStorage.getItem('totalUserScore');
+//sort the table to put top scores on top
+function sortTable() {
+    let table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("gameResultsTable");
+    switching = true;
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[2];
+            y = rows[i + 1].getElementsByTagName("TD")[2];
+            if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
 }
-
-setUserScore()
