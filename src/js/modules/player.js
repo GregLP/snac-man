8aee157b89281981dd05a7b63488747047836e22
greg <greglp@users.noreply.github.com>
2022-06-12 13:35:18 -0700

    The player profile page now uses a table to display game results

--- src/js/modules/player.js
+++ src/js/modules/player.js
@@ -3,30 +3,33 @@ let allGames = Object.values(JSON.parse(localStorage.getItem('playerGames')));
 for (let i = 0; i < allGames.length; i++) {
     let gameValue = allGames[i];
     if (gameValue.hasOwnProperty('played')) {
-        createHeader(allGames[i]['title']);
-        createWinLossPara(allGames[i]['result']);
-        createScorePara(allGames[i]['score']);
+        let tableRow = document.createElement('tr');
+        tableRow.setAttribute('id', `results${i}`);
+        tableRow.appendChild(createHeader(allGames[i]['title']));
+        tableRow.appendChild(createWinLossPara(allGames[i]['result']));
+        tableRow.appendChild(createScorePara(allGames[i]['score']));
+        document.getElementById('gameResultsTableBody').appendChild(tableRow);
     }
 }
 
 function createHeader(title) {
-    let heading = document.createElement('h2');
+    let heading = document.createElement('th');
     heading.innerHTML = title;
-    document.getElementById('gameResults').appendChild(heading);
+    return heading;
 }
 
 function createWinLossPara(result) {
-    let resultPara = document.createElement('p');
+    let resultPara = document.createElement('th');
     if (result === 'win') {
         resultPara.innerHTML = `You Won!`;
     } else {
         resultPara.innerHTML = `You Lost!`;
     }
-    document.getElementById('gameResults').appendChild(resultPara);
+    return resultPara;
 }
 
 function createScorePara(score) {
-    let scorePara = document.createElement('p');
+    let scorePara = document.createElement('th');
     scorePara.innerHTML = score;
-    document.getElementById('gameResults').appendChild(scorePara);
+    return scorePara;
 }
